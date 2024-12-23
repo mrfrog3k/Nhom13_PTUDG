0c18af2bb7100ecdbcc61ec0af9078350fbb3f38
mrfrog3k <125977099+mrfrog3k@users.noreply.github.com>
2024-12-23 18:10:34 +0700

    Add files via upload

---
+++ HappyDino_project/Assets/Script/LaserScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    //1
+    public Sprite laserOnSprite;
+
+    public Sprite laserOffSprite;
+
+    //2
+    public float toggleInterval = 0.5f;
+
+    public float rotationSpeed = 0.0f;
+
+    //3
+    private bool isLaserOn = true;
+
+    private float timeUntilNextToggle;
+
+    //4
+    private Collider2D laserCollider;
+
+    private SpriteRenderer laserRenderer;
+
+    // Use this for initialization
+    private void Start()
+    {
+        //1
+        timeUntilNextToggle = toggleInterval;
+        //2
+        laserCollider = gameObject.GetComponent<Collider2D>();
+        laserRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //1
+        timeUntilNextToggle -= Time.deltaTime;
+
+        //2
+        if (timeUntilNextToggle <= 0)
+        {
+            //3
+            isLaserOn = !isLaserOn;
+
+            //4
+            laserCollider.enabled = isLaserOn;
+
+            //5
+            if (isLaserOn)
+            {
+                laserRenderer.sprite = laserOnSprite;
+            }
+            else
+            {
+                laserRenderer.sprite = laserOffSprite;
+            }
+
+            //6
+            timeUntilNextToggle = toggleInterval;
+        }
+
+        //7
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ HappyDino_project/Assets/Script/ParallaxScroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScroll : MonoBehaviour
+{
+    //1
+    public Renderer background;
+
+    public Renderer foreground;
+
+    //2
+    public float backgroundSpeed = 0.02f;
+
+    public float foregroundSpeed = 0.06f;
+
+    //3
+    public float offset = 0.0f;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float backgroundOffset = offset * backgroundSpeed;
+        float foregroundOffset = offset * foregroundSpeed;
+
+        background.material.mainTextureOffset = new Vector2(backgroundOffset, 0);
+        foreground.material.mainTextureOffset = new Vector2(foregroundOffset, 0);
+    }
+}
